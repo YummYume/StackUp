9dff8eca58de7f54574e92d72a0e10a42d032c89
spacelocust <lucasboganin@hotmail.fr>
2023-02-23 18:25:45 +0000

    feat: add prod config

--- deployement.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget https://raw.githubusercontent.com/YummYume/StackUp/master/docker-compose.prod.yml
-
-docker login -u $(DOCKER_USERNAME) -p $(DOCKER_TOKEN)
-docker compose pull symfony-app
-docker compose up -d --no-deps
-docker logout
